e5c9a1a9fd3f0d3955488a502ee9e2b15321d2ad
yashweblife <yashweblife20@gmail.com>
2023-04-11 20:45:18 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.learnmagiciovue',
+  appName: 'learnmagiciovue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
